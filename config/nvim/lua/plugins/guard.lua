fc4b714ebc1e8f0a96f38fc9e5dfc179317e9039
moddong <lxwyylm@gmail.com>
2023-12-27 20:26:12 +0800

    update

--- config/nvim/lua/plugins/guard.lua
+++ config/nvim/lua/plugins/guard.lua
@@ -3,11 +3,9 @@ local ft = require("guard.filetype")
 ft("c"):fmt("clang-format")
 ft("cpp"):fmt("clang-format")
 ft("lua"):fmt("stylua")
+ft("rust"):fmt("rustfmt")
 ft("python"):fmt("black")
 ft("go"):fmt("lsp"):append("golines")
 ft("javascript,typescript,javascriptreact"):fmt("prettier")
 
-require("guard").setup({
-  fmt_on_save = true,
-  lsp_as_default_formatter = false,
-})
+require("guard").setup()
