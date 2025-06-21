054c2e26787c438e728aa92774ceed579e4a4e8b
moddong <lxwyylm@gmail.com>
2025-06-21 16:33:01 +0800

    update

---
+++ config/nvim/lua/plugins/editor/minifiles.lua
@@ -0,0 +1,63 @@
+return {
+  "echasnovski/mini.files",
+  version = "*",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        local minifiles = require("mini.files")
+        if vim.bo.ft == "minifiles" then
+          minifiles.close()
+        else
+          local file = vim.api.nvim_buf_get_name(0)
+          local file_exists = vim.fn.filereadable(file) ~= 0
+          minifiles.open(file_exists and file or nil)
+          minifiles.reveal_cwd()
+        end
+      end,
+      { desc = "Open current directory" },
+    },
+    { "<leader>we", "<CMD>lua MiniFiles.open()<CR>", { desc = "Open working directory" } },
+  },
+  opts = {
+    content = {
+      filter = function(entry)
+        return entry.name ~= ".git"
+      end,
+      sort = function(entries)
+        local all_paths = table.concat(
+          vim
+            .iter(entries)
+            :map(function(entry)
+              return entry.path
+            end)
+            :totable(),
+          "\n"
+        )
+        local output_lines = {}
+        local job_id = vim.fn.jobstart({ "git", "check-ignore", "--stdin" }, {
+          stdout_buffered = true,
+          on_stdout = function(_, data)
+            output_lines = data
+          end,
+        })
+
+        -- command failed to run
+        if job_id < 1 then
+          return entries
+        end
+
+        -- send paths via STDIN
+        vim.fn.chansend(job_id, all_paths)
+        vim.fn.chanclose(job_id, "stdin")
+        vim.fn.jobwait({ job_id })
+        return require("mini.files").default_sort(vim
+          .iter(entries)
+          :filter(function(entry)
+            return not vim.tbl_contains(output_lines, entry.path)
+          end)
+          :totable())
+      end,
+    },
+  },
+}
---
+++ config/nvim/lua/plugins/editor/phoenix.lua
@@ -0,0 +1,16 @@
+vim.g.phoenix = {
+	dict = {
+		capacity = 100000,
+		min_word_length = 4,
+		word_pattern = "[^%s%.:%(%)%[%]%{%}%d]+",
+	},
+	scanner = {
+		scan_batch_size = 1000, -- Scan 1000 items per batch
+		cache_duration_ms = 5000, -- Cache results for 5s
+		throttle_delay_ms = 5000, -- Wait 5s between updates
+		ignore_patterns = {}, -- Dictionary or file ignored when path completion
+	},
+}
+return {
+	"nvimdev/phoenix.nvim",
+}
