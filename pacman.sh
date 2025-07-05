3f75565c2dd044bdb9fb4014216d290ecb0129e0
moddong <lxwyylm@gmail.com>
2025-07-05 22:50:15 +0800

    update

--- pacman.sh
+++ pacman.sh
@@ -1,17 +1,37 @@
 #! /usr/bin/bash
 
 
-PKGLIST=()
 
-sudo pacman -S git base-devel
-sudo pacman  --noconfirm -Syu zsh-autosuggestions  zsh-completions  zsh-syntax-highlighting \
-  python3 python-pip python-msgpack python-black python-ruff cmake ninja gcc clang curl unzip jq ripgrep proxychains \
+sudo pacman -Syu git base-devel zsh-autosuggestions  zsh-completions  zsh-syntax-highlighting \
+  python3 python-pip python-msgpack python-black python-ruff cmake ninja gcc clang curl unzip jq ripgrep \
   tree-sitter nodejs npm go gopls lua-language-server stylua fzf lazygit
 
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+[ -e ${CARGO_HOME} ] || mkdir -vp ${CARGO_HOME:-$XDG_DATA_HOME/cargo}
+cat << EOF | tee -a ${CARGO_HOME:-$XDG_DATA_HOME}/config.toml
+[source]
+crates-io = { replace-with = "rsproxy-sparse" }
+rsproxy-sparse = { registry = "sparse+https://rsproxy.cn/index/" }
+EOF
+
+
 rustup default stable
 rustup component add rust-analyzer
+cargo install cargo-shear cargo-expand cargo-msrv cargo-edit cargo-supply-chain  cargo-deny cargo-audit cargo-depgraph cargo-update cargo-cache cargo-xwin cargo-zigbuild cargo-wizard cargo-binstall cargo-get cargo-workspace-unused-pub cargo-insta cargo-autoinherit  cargo-bloat
+
+
+
+do
+  git clone https://aur.archlinux.org/paru.git
+  cd paru
+  makepkg -si
+util [[ -x "/usr/bin/paru" ]]
+done
+rm -rf paru
+
+if [[ -x "/usr/bin/paru" ]]; then
+  paru -Sy --needed linuxqq-nt-bwrap thunderbird wechat-universal-bwrap wemeet-bin qemu-full pipewir wireplumber pipewire-pulse pipewire-alsa pipewire-jack shfmt gopls biome cliphist dbeaver sqlitebrowser exa postgresql sqlite rsync parallel-disk-usage bottom yazi ffmpegthumbnailer unarchiver ffmpeg man-db man-pages kdiff3 dos2unix choose bat fd hexyl libtree zip unrar 7zip viu imagemagick mupdf-tools gwenvied zathura poppler wps-office-cn wps-office-mui-zh-cn ttf-wps-fonts wps-office-fonts openssh lsof gping rustscan asciinema screenkey macchina erdtree dysk bottom acpi bandwhich obs-studio pueue gwenview inkscape gmp pot-translation procs navi qbittorrent gimp atuin microsoft-edge-stable-bin microsoft-edge-stable-bin
+fi
 
 npm config set registry https://registry.npmmirror.com/
 sudo npm config set registry https://registry.npmmirror.com/
@@ -22,10 +42,3 @@ go env -w GOPROXY=https://goproxy.cn,direct
 sudo go env -w GO111MODULE=on
 sudo go env -w GOPROXY=https://goproxy.cn,direct
 sudo npm i -g bash-language-server pyright vscode-langservers-extracted typescript typescript-language-server
-[ -e ${CARGO_HOME} ] || mkdir -vp ${CARGO_HOME:-$XDG_DATA_HOME/cargo}
-cat << EOF | tee -a ${CARGO_HOME:-$XDG_DATA_HOME}/config.toml
-[source]
-crates-io = { replace-with = "rsproxy-sparse" }
-rsproxy-sparse = { registry = "sparse+https://rsproxy.cn/index/" }
-
-EOF
